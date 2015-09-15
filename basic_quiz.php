a07b1746d15b81bb097c4478b588e007de537c86
anupsamantaray <anup.bapu@gmail.com>
2015-09-16 04:23:38 +0530

    commit

--- basic_quiz.php
+++ basic_quiz.php
@@ -37,42 +37,54 @@
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										11.5m
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										12.5m
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										8m
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										6m
 									</label>
 								</li>
 							</ul>
 						</div>
 						<div class="total_quiz">
-							<p class="quiz">1. A person throws a ball vertically upward with an initial velocity of 15 m/s.How high it goes?</p>
+							<p class="quiz">2.A body, whose momentum is constant, must have constant-</p>
 							<ul>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Force
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Velocity
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Acceleration
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										All of these
 									</label>
 								</li>
 							</ul>
@@ -80,47 +92,88 @@
 						
 						
 						<div class="total_quiz">
-							<p class="quiz">1. A person throws a ball vertically upward with an initial velocity of 15 m/s.How high it goes?</p>
+							<p class="quiz">3.A man getting down a running bus, falls forward because-</p>
 							<ul>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										due to inertia of rest, road is left behind and man reaches forward
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										due to inertia of motion upper part of body continues to be in motion in forward direction while feet come to rest as soon as they touch the road
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										he leans forward as a matter of habit
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										of the combined effect of all the three factors stated in (1), (2) and (3)
 									</label>
 								</li>
 							</ul>
 						</div>
 						<div class="total_quiz">
-							<p class="quiz">1. A person throws a ball vertically upward with an initial velocity of 15 m/s.How high it goes?</p>
+							<p class="quiz">4. Which of the following class of forces is different from others -</p>
 							<ul>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Pulling of a cart
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Stretching of a coiled spring
 									</label>
 								</li>
 								<li>
 									<label>
 										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
+										Kicking of a football
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										Electrical force
+									</label>
+								</li>
+							</ul>
+						</div>
+						<div class="total_quiz">
+							<p class="quiz">5.The speed of a falling body increases continuously, this is because  -</p>
+							<ul>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										No force acts on it
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										It is very light
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										The air exert the frictional force
+									</label>
+								</li>
+								<li>
+									<label>
+										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+										The earth attract it
 									</label>
 								</li>
 							</ul>
--- competitive_quiz.php
+++ competitive_quiz.php
@@ -22,67 +22,254 @@
 </script>
 <div class="container">
 	<div class="row">
-            <div class="col-md-12">  
-             <div class="schocenter">
-                <div class="arrow">
-                      <img src="images/center-1.png"></div>
-                      <h4 class="border1">Basic Quiz</h4>
-            		</div>
-                 <div class="scholourcenter">
-					<div class="col-md-12">
-						<h2 class="texttitle">For more quizzes <a href="#s"> Click Here</a></h2>
-						<div class="total_quiz">
-							<p class="quiz">1. A person throws a ball vertically upward with an initial velocity of 15 m/s.How high it goes?</p>
-							<ul>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-							</ul>
-						</div>
-						<div class="total_quiz">
-							<p class="quiz">1. A person throws a ball vertically upward with an initial velocity of 15 m/s.How high it goes?</p>
-							<ul>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-								<li>
-									<label>
-										<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
-										Option one is this and that
-									</label>
-								</li>
-							</ul>
-						</div>
-						<div class="clr"></div>
+		<div class="col-md-12">  
+			<div class="schocenter">
+				<div class="arrow">
+					<img src="images/center-1.png">
+				</div>
+				<h4 class="border1">Compitative Quiz</h4>
+			</div>
+			<div class="scholourcenter">
+				<div class="col-md-12">
+					<h2 class="texttitle">For more quizzes <a href="#"> Click Here</a></h2>
+					<div class="total_quiz">
+						<p class="quiz">1. Directions A person throws a ball vertically upward with an initial velocity of 15 m/s.How long the ball is in air before it comes to his hand.?</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									2.0 s
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									1.0 s
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									3.06 s
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									5.01 s
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">2. When a body is stationary?</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									There is no force acting on it
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									The force acting on it not in contact with it
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									The combination of forces acting on it balances each other
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									The body is in vacuum
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">3.  A fish is swimming upward at an angle of 30 deg with the horizontal. The direction of the force of gravity acting on it is -</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									upward
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									downward
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									horizontal
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									at an angle upward
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">4. Condensation and evaporation are:</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									There is no force acting on it
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Irreversible change
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Desirable change
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									None of these
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">5.Solubility is affected by:</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Temperature
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Pressure
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Both
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									None of these	
+								</label>
+							</li>
+						</ul>
+					</div>
+					
+					<div class="total_quiz">
+						<p class="quiz">6.Solubility is affected by:</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Temperature
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Pressure
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Both
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									None of these	
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">6.Schiff's reagent gives pink colour with:</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Acetaldehyde
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Formaldehyde
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Acetone
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									None of these	
+								</label>
+							</li>
+						</ul>
+					</div>
+					<div class="total_quiz">
+						<p class="quiz">7.Solubility is affected by:</p>
+						<ul>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Temperature
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Pressure
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									Both
+								</label>
+							</li>
+							<li>
+								<label>
+									<input name="optionsRadios" id="optionsRadios1" value="option1" checked="" type="radio">
+									None of these	
+								</label>
+							</li>
+						</ul>
 					</div>
 					<div class="clr"></div>
 				</div>
-</div>
-</div>
-</div>
+				<div class="clr"></div>
+			</div>
+		</div>
+	</div>
 </div>
 <?php include 'footer.php';?>
